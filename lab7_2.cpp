3d07ac535677400e256c07d401d50069f4c81d6a
Vasiree Nuntajan <wasiree098@gmail.com>
2022-12-14 21:07:41 +0700

    lab7-2 complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -2,13 +2,32 @@
 
 using namespace std;
 
-int main(){
-	
+int main()
+{
 	int i = 0, P[5000], k, result;
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
 	}
-	
+	cout << "Please input k: ";
+	cin >> k;
+	if (k>0)
+	{
+	    i=0;
+	    result = 0;
+	    while (i<5000)
+	    {
+	        if (P[i]%k == 0)
+	        {
+	            result = result - P[i];
+            }else{
+                result = result + P[i];
+            }   
+            i++;
+        }
+        cout << "Result = "<< result;
+    }else{
+        cout << "Invalid input!!!";
+    }
 	return 0;
 }
