a9656e0a135247edfa94514ac4943dd43a30c21c
Vasiree Nuntajan <wasiree098@gmail.com>
2022-12-14 21:10:43 +0700

    lab7-3 complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
-
-int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+int adiff(int A,int B){
+    int c;
+    c = A-B;
+    c = abs(c);
+    if (c%360<=180){
+    return c%360;
+    }else{
+    return 360-(c%360);
+    }
 }
